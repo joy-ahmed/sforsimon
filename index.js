872c4148a49321818e66032e3812110aea5ca9fa
Joy Ahmed <dev.joyahmed@gmail.com>
2023-09-10 06:07:24 +0600

    added swagger ui basics

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const format = require("date-format");
-
 const app = express();
+
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const YAML = require("yaml");
+
+const file = fs.readFileSync("./swagger.yaml", "utf8");
+const swaggerDocument = YAML.parse(file);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 PORT = process.env.PORT || 9000;
 
